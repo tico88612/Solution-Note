4ec06201eeb5345f793940051af12344caca7ba3
Yang Cheng-Hao <tico88612@gmail.com>
2019-11-13 14:32:54 +0800

    Add some Solutions

---
+++ TIOJ/TIOJ 1818.cpp
@@ -0,0 +1,13 @@
+#include<cstdio>
+
+/*你第一行的答案(目前為範例輸出)*/
+const char ans1[100000] = "y94bp4ck6y vmp41u3n945j/ ji3u62u/4cjo4y3vu4m,42j6wu6gaj4ru6gj bj4gj tj z; g41u/4fm,4g6yji42l4b06c.4g6ru4hk4g4t/6g4g4z.3s/6t/6ej/ 2k7gj bj4gj tj ru6wu6aj4ul fu.62;304gj bj4gj tj ji3u62u/4cjo4ru412k6ru8 g;42j6vu,32;353xu/4xu/4j94ji3cjo4ru42k6g0 2ul4nji3u3tj6hji4m/42k7gj tj 1l ej8 504wu/653xu/4ji3cjo4rup4xu;45/ fm31j4zp4zp gj4m3fu6dj/45k71j4bj6m/4ru83ru,3cji42k6rup4xu;4el 2k7zp gj4ru6g3j06fm0616cjo4vu,3ru.4njo61u04gj tj u45j/3ck6z83gj tj yjo4c.4y941u3g4ji3cjo4rup4xu;4ej/ 5/32k7y4ru gj vu,31j6ul4eo3bp6wul al61u/42k7ru cjo4";
+
+/*你第二行的答案(目前為範例輸出)*/
+const int ans2 = 12345;
+
+int main()
+{
+    puts(ans1);
+    printf("%d\n",ans2);
+}
